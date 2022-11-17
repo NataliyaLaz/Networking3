f6b859c690a8711c309b4872340bc1cb7a7fd3eb
Nataliya Lazouskaya <93728977+NataliyaLaz@users.noreply.github.com>
2022-11-17 12:24:48 +0300

    Initial Commit

---
+++ Networking3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Networking3
+//
+//  Created by Nataliya Lazouskaya on 17.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
